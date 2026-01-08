190c8379e78de31f8b963762c53af13d81156f55
HoneyPaptan <hpaptan@gmail.com>
2026-01-08 20:55:36 +0530

    fixed postcss error

--- components/podcast-flow.tsx
+++ components/podcast-flow.tsx
@@ -17,7 +17,6 @@ import {
   Handle,
   Position,
 } from "@xyflow/react"
-import "@xyflow/react/dist/style.css"
 import { Card } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import {
